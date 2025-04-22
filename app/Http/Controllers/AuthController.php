87b28f8ca9de632e4f876092e20e0e0e8f035bb1
JDRodriguez80 <jesus1280rodriguez@gmail.com>
2025-04-22 12:23:19 -0500

    controllador de registro y verificacion completo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Otp;
+use App\Models\Usuario;
 use App\Services\TwilioService;
 use Illuminate\Http\Request;
 
@@ -31,9 +32,45 @@ class AuthController extends Controller
         }
 
     }
-
-    public function autenticar()
+    //verificando el codiogo y telefono
+    public function verifyOtp(Request $request)
     {
+        $request->validate([
+            'telefono' => 'required|string',
+            'codigo_otp' => 'required|string',
+        ]);
 
+        $registro = Usuario::where('telefono',$request->telefono)
+            ->where('codigo_otp', $request->codigo_otp)
+            ->where('expira_en','>',now())
+            ->first();
+        if(!$registro){
+            return response()->json(['error'=>'codigo invalido o expirado'],401);
+        }
+        return response()->json(['message'=>'codigo valido'], 200);
+    }
+
+    //registrando al usuario
+    public function registrar(Request $request)
+    {
+        $request->validate([
+            'telefono' => 'required|string|unique:usuarios',
+            'nombre' => 'required|string',
+            'apellido' => 'required|string',
+            'password' => 'required|string',
+            'nick' => 'required|string|unique:usuarios',
+            'email' => 'required|email|unique:usuarios',
+            'direccion' => 'required|string',
+        ]);
+        $usuario = Usuario::create([
+            'telefono' => $request->telefono,
+            'nombre' => $request->nombre,
+            'apellido' => $request->apellido,
+            'nick' => $request->nick,
+            'email' => $request->email,
+            'direccion' => $request->direccion,
+            'password' => bcrypt($request->password),
+        ]);
+        return response()->json(['message' => 'Registro exitoso', 'usuario' => $usuario]);
     }
 }
